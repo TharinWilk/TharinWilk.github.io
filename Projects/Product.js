d8b4eb53331e8fb2be13d50bd77b2beafbb4b731
Tharin <Tharin.m.wilk@gmail.com>
2021-12-05 17:43:49 -0500

    Updated all files before pushing to github

--- Projects/Product.js
+++ Projects/Product.js
@@ -1,3 +1,5 @@
+
+// Navbar
 const toggleNavbar = document.querySelector('.toggle-navbar');
 const navList = document.querySelector('.nav-list');
 
@@ -29,46 +31,64 @@ const skillsLink = document.querySelector('#skills-link');
 const projectLink = document.querySelector('#project-link');
 const contactLink = document.querySelector('#contact-link');
 
+const body = document.querySelector('body');
+
+skillsPage.style.transform = "translateX(-100%)";
+projectPage.style.transform = "translateY(100%)";
+contactPage.style.transform = "translateX(100%)";
+
+let overflowY = "overflow-y";
 let skills = "hidden";
 let projects = "hidden";
 let contacts = "hidden";
 
 function resetScreens() {
   skillsPage.style.transform = "translateX(-100%)";
+  projectPage.style.visibility = "visible";
   projectPage.style.transform = "translateY(100%)";
-  contactPage.style.right = "-300px";
+  contactPage.style.transform = "translateX(100%)";
+  body.style.overflowY = "hidden";
 }
 
 landingLink.addEventListener('click', () => {
   resetScreens();
-})
+});
 
 skillsLink.addEventListener('click', () => {
   resetScreens();
   if(skills == "hidden") {
     skillsPage.style.transform = "translateX(0%)";
     skills = "open"
+    projects = "hidden"
+    contacts = "hidden"
+    body.style.overflowY = "auto";
+    projectPage.style.visibility = "hidden";
   } else {
     skills = "hidden"
   }
-})
+});
 
 projectLink.addEventListener('click', () => {
   resetScreens();
   if(projects == "hidden") {
     projectPage.style.transform = "translateY(0%)";
     projects = "open"
+    skills = "hidden"
+    contacts = "hidden"
+    body.style.overflowY = "auto";
   } else {
     projects = "hidden"
   }
-})
+});
 
 contactLink.addEventListener('click', () => {
   resetScreens();
   if(contacts == "hidden") {
-    contactPage.style.right = "0px";
+    contactPage.style.transform = "translateX(0%)";
     contacts = "open"
+    skills = "hidden"
+    projects = "hidden"
   } else {
     contacts = "hidden"
   }
-})
+});
